ab85664984a13b4d384e0f9038cee41b65ed0f70
Alastair Taft <ali.c.taft@gmail.com>
2016-09-07 19:20:24 +1000

    Removed the code option for under the blocks menu as this was throwing a wrapper property of underfined runtime error.

--- src/components/SideControl/MoreOptions.js
+++ src/components/SideControl/MoreOptions.js
@@ -153,7 +153,7 @@ export default class MoreOptions extends Component {
 				    <path d="M0 0h24v24H0z" fill="none"/>
 					</svg>
 				</div>
-				<div
+				{/*<div
 					style={Object.assign({}, 
 						styles.iconContainer,
 						this.props.selectedBlockType == 'code' 
@@ -169,7 +169,7 @@ export default class MoreOptions extends Component {
 				    <path d="M0 0h24v24H0V0z" fill="none"/>
 				    <path d="M9.4 16.6L4.8 12l4.6-4.6L8 6l-6 6 6 6 1.4-1.4zm5.2 0l4.6-4.6-4.6-4.6L16 6l6 6-6 6-1.4-1.4z"/>
 					</svg>
-				</div>
+				</div>*/}
 			</div>
 		</div>
 	}

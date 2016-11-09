d20d13182c078fb7169b473c6c8ca63328c30d0b
Alastair Taft <ali.c.taft@gmail.com>
2016-11-10 10:54:50 +1100

    Added defaultBlockButtons to the main package export

--- src/Editor.js
+++ src/Editor.js
@@ -1,5 +1,6 @@
 
 import Editor from './components/Editor'
 export defaultInlineButtons from './components/PopoverControl/defaultButtons.js'
+export defaultBlockButtons from './components/SideControl/defaultButtons.js'
 export defaultDecorator from './components/defaultDecorator.js'
 export default Editor

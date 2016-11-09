20de65bf5e7de4852a9cc0049caf71a81d64b396
Alastair Taft <ali.c.taft@gmail.com>
2016-11-10 10:57:51 +1100

    Added insertBlock to the default package exports.

--- src/Editor.js
+++ src/Editor.js
@@ -3,4 +3,5 @@ import Editor from './components/Editor'
 export defaultInlineButtons from './components/PopoverControl/defaultButtons.js'
 export defaultBlockButtons from './components/SideControl/defaultButtons.js'
 export defaultDecorator from './components/defaultDecorator.js'
+export insertBlock from './modifiers/insertBlock'
 export default Editor

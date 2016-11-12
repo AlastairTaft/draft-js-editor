972aa23085f23a8fa61cb2357db041c01aaa43e8
Alastair Taft <ali.c.taft@gmail.com>
2016-11-13 09:56:07 +1100

    Exposed insertAtomicBlock modified

--- src/Editor.js
+++ src/Editor.js
@@ -4,4 +4,5 @@ export defaultInlineButtons from './components/PopoverControl/defaultButtons.js'
 export defaultBlockButtons from './components/SideControl/defaultButtons.js'
 export defaultDecorator from './components/defaultDecorator.js'
 export insertBlock from './modifiers/insertBlock'
+export insertAtomicBlock from './modifiers/insertAtomicBlock'
 export default Editor

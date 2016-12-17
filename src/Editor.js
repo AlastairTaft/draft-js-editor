2d013386ab78acd8b291dd66b4cda753e673acb3
Alastair Taft <ali.c.taft@gmail.com>
2016-12-17 22:45:28 +1100

    Test adding file to public folder

--- src/Editor.js
+++
@@ -1,5 +0,0 @@
-
-import Editor from './components/Editor'
-export defaultInlineButtons from './components/PopoverControl/defaultButtons.js'
-export defaultDecorator from './components/defaultDecorator.js'
-export default Editor
--- src/lib/generateUniqueType.js
+++
@@ -1,5 +0,0 @@
-var uniqueStringCounter_ = Math.random() * 0x80000000 | 0;
-
-export default function(){
-	return 'type' + uniqueStringCounter_++
-}

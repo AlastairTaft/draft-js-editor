78238c960f1941c24d9d44b404ff19dc85166584
Alastair Taft <ali.c.taft@gmail.com>
2017-04-12 23:45:03 +1000

    Added insertAtomicBlock to the default export.

--- src/Editor.js
+++ src/Editor.js
@@ -3,4 +3,5 @@ import Editor from './components/Editor'
 export defaultInlineButtons from './components/PopoverControl/defaultButtons.js'
 export defaultBlockButtons from './components/SideControl/defaultButtons.js'
 export defaultDecorator from './components/defaultDecorator.js'
+export insertAtomicBlock from './modifiers/insertAtomicBlock'
 export default Editor
